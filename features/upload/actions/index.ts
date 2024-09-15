a9e4e8dbfe10f70f6dc104d743444cc17735ef77
acrostm <zjc971109@gmail.com>
2024-09-15 12:44:02 -0700

    [FEAT] Now using R2 public url to show image And now using compress image to webp for all image over 1MB

--- features/upload/actions/index.ts
+++ features/upload/actions/index.ts
@@ -1,7 +1,6 @@
 "use server";
 
-import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import imageType from "image-type";
 import path from "node:path";
 import sharp from "sharp";
@@ -10,6 +9,7 @@ import { R2_BUCKET, R2_UPLOAD_DIR } from "@/config";
 
 import { ERROR_NO_PERMISSION } from "@/constants";
 import { noPermission } from "@/features/user";
+import { createCuid } from "@/lib/cuid";
 import { s3 } from "@/lib/r2-storage";
 
 const getImageInfo = async (file: File) => {
@@ -24,47 +24,52 @@ const getImageInfo = async (file: File) => {
   };
 };
 
-// 如果不是图片，原样返回，是图片返回压缩后的图片路径
 const compressImage = async (file: File): Promise<Buffer> => {
   const buffer = await file.arrayBuffer();
   const { isGif, isImage, isWebp } = await getImageInfo(file);
 
+  // 如果不是图片，或者已经是 WebP 格式，直接返回原始文件数据
   if (!isImage || isWebp) {
     return Buffer.from(buffer);
   }
 
-  let animated = false;
-  if (isGif) {
-    animated = true;
-  }
+  // 如果是 GIF 动图，设置 animated 为 true
+  const options = isGif ? { animated: true } : {};
 
-  return sharp(Buffer.from(buffer), { animated, limitInputPixels: false })
-    .webp({ lossless: true })
-    .toBuffer();
+  try {
+    // 使用 sharp 将图像转换为 WebP 格式
+    return await sharp(Buffer.from(buffer), options)
+      .webp({ quality: 66, lossless: false }) // 设置质量和无损/有损压缩
+      .toBuffer();
+  } catch (error) {
+    throw new Error(`Failed to compress image`);
+  }
 };
 
-const uploadToR2 = async (file: File, compressedFile: Buffer) => {
+const uploadToR2 = async (
+  file: File,
+  compressedFile: Buffer,
+  useWebp: boolean,
+) => {
+  // 使用 WebP 格式时，手动设置扩展名为 .webp
+  const fileExtension = useWebp ? ".webp" : path.extname(file.name);
+
   const key = `${R2_UPLOAD_DIR}${new Date().toISOString().split("T")[0]}/${
-    file.name
+    createCuid() + fileExtension
   }`;
 
   const uploadParams = {
     Bucket: R2_BUCKET,
     Key: key,
     Body: compressedFile,
-    ContentType: getContentType(file.name),
+    // 根据实际的压缩格式来确定 Content-Type
+    ContentType: useWebp ? "image/webp" : getContentType(file.name),
   };
 
   const command = new PutObjectCommand(uploadParams);
   await s3.send(command);
 
-  // Generate a signed URL that's valid for 1 hour (3600 seconds)
-  const getObjectParams = { Bucket: R2_BUCKET, Key: key };
-  const getCommand = new GetObjectCommand(getObjectParams);
-  const signedUrl = await getSignedUrl(s3, getCommand, { expiresIn: 3600 });
-
-  // Otherwise, return the signed URL
-  return signedUrl;
+  return `https://r2.jiachzha.com/${key}`;
 };
 
 const getContentType = (fileName: string): string => {
@@ -97,23 +102,36 @@ export const uploadFile = async (
     return { error: "No file found" };
   }
 
-  const { isImage } = await getImageInfo(file);
+  const { isImage, isWebp } = await getImageInfo(file);
   if (!isImage) {
     return { error: "Uploaded file is not an image" };
   }
 
   const fileSize = file.size;
-  const sizeLimit = 1024 * 1024 * 10; // 10MB
+  const sizeLimit = 1024 * 1024 * 30; // 30MB
 
   if (fileSize > sizeLimit) {
     return { error: "File size too large" };
   }
 
-  try {
-    const compressedFile = await compressImage(file);
-    const signedUrl = await uploadToR2(file, compressedFile);
-    return { url: signedUrl };
-  } catch (error) {
-    return { error: "Upload failed" };
+  if (!(isWebp || fileSize < 1024 * 1024)) {
+    try {
+      const compressedFile = await compressImage(file);
+      const uploadedUrl = await uploadToR2(file, compressedFile, true); // 使用 WebP 格式
+      return { url: uploadedUrl };
+    } catch (error) {
+      return { error: "Upload failed" };
+    }
+  } else {
+    try {
+      const uploadedUrl = await uploadToR2(
+        file,
+        Buffer.from(await file.arrayBuffer()),
+        false,
+      );
+      return { url: uploadedUrl };
+    } catch (error) {
+      return { error: "Upload failed" };
+    }
   }
 };
